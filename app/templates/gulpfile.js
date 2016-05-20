689165c55fa1ffcef98c78a60255412e1f2fc46e
Ryan Smith <ryan.smith@stormid.com>
2016-05-20 14:52:02 +0100

    Copied over the scaffold

---
+++ app/templates/gulpfile.js
@@ -0,0 +1,241 @@
+// generated on <%= date %> using <%= name %> <%= version %>
+
+/*global require*/
+/* Require the gulp and node packages */
+var gulp = require('gulp'),
+    pkg = require('./package.json'),
+    del = require('del'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    header = require('gulp-header'),
+    pixrem = require('gulp-pixrem'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    minifyCss = require('gulp-minify-css'),
+    swig = require('gulp-swig'),
+    frontMatter = require('gulp-front-matter'),
+    data = require('gulp-data'),
+    pagespeed = require('psi'),
+    extname = require('gulp-extname'),
+    sourcemaps = require('gulp-sourcemaps'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    path = require('path'),
+    browserify = require('browserify'),
+    watchify = require('watchify'),
+    cache = require('gulp-cache'),
+    imagemin = require('gulp-imagemin'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber'),
+    debug = require('gulp-debug'),
+    runSequence = require('run-sequence'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    jshint = require('gulp-jshint'),
+    jshintConfig = pkg.jshintConfig;
+
+
+/* Set up the banner */
+var banner = [
+
+].join('\n');
+
+/* Autoprefixer settings */
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 9',
+  'ie_mob >= 10',
+  'ff >= 20',
+  'chrome >= 4',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
+/* Build destination */
+var outputDir = './build';
+
+/* Where CSS, JS, imgs are piped */
+var assetPath = '/content';
+
+/* Source files for the pipe */
+var src = {
+	css: './src/scss/',
+	js: './src/js/',
+	html: './src/templates/',
+    img: './src/img/'
+};
+
+/* Destination for the build */
+var dest = {
+	css: outputDir + assetPath + '/css/',
+	js:  outputDir + assetPath + '/js/',
+	html: outputDir,
+    img: outputDir + assetPath + '/img/',
+    fonts: outputDir + assetPath + '/fonts/'
+};
+
+/* Set the PSI variables */
+var publicUrl = '', //publicly accessible URL on your local machine, demo, staging, live...
+    psiStrategy = 'mobile'; //'mobile' or 'desktop'
+
+/* Error notificaton*/
+var onError = function(err) {
+    notify.onError({
+        title:    "Gulp",
+        subtitle: "Failure!",
+        message:  "Error",
+        sound:    "Beep"
+    })(err);
+
+    this.emit('end');
+};
+
+/************************
+ *  Task definitions 
+ ************************/
+/* Lint JS */
+gulp.task('lint', function() {
+	return gulp.src(src.js)
+		.pipe(jshint(jshintConfig))
+		.pipe(jshint.reporter('default'));
+});
+
+gulp.task('js:browserify', function () {
+  var b = browserify({
+    entries: src.js + 'app.js',
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(dest.js));
+});
+
+gulp.task('js:async', function () {
+    return gulp.src(src.js + 'async/**/*')
+  		.pipe(uglify())
+  		.pipe(rename({suffix: '.min'}))
+  		.pipe(gulp.dest(dest.js + 'async/'));
+});
+gulp.task('js', ['js:browserify', 'js:async']);
+
+/* Build the flat html */
+gulp.task('html', function(){
+    return gulp.src(src.html + 'views/**/*.html')
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(frontMatter({ property: 'data' }))
+        .pipe(data(function(file) {
+            return {'assetPath': assetPath};
+        }))
+        .pipe(swig({
+            defaults: {
+                cache: false
+            }
+        }))
+      .pipe(gulp.dest(dest.html));
+});
+
+/* 
+ * SASS > CSS
+ * Build CSS from scss, prefix and add px values from rem
+ *
+ */
+gulp.task('sass', function () {
+    return gulp.src([src.css + '**/*.scss', '!' + src.css + '{fonts,kss}/*.*'])
+		.pipe(plumber({errorHandler: onError}))
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
+		.pipe(pixrem())
+		// inter line
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(dest.css));
+});
+
+gulp.task('css', ['sass']);
+
+/* Optimize images */
+gulp.task('img', function () {
+    return gulp.src([src.img + '**/*'])
+        .pipe(imagemin({
+          progressive: true,
+          interlaced: true,
+          svgoPlugins: [{removeViewBox: true}]
+        }))
+        .pipe(gulp.dest(dest.img));
+});
+
+/* Fonts */
+gulp.task('font', function() {
+    return gulp.src(src.fonts + '**/*.*')
+        .pipe(gulp.dest(dest.fonts));
+});
+
+/* Compress js */
+gulp.task('compress:js', function() {
+	return gulp.src(dest.js + 'app.js')
+		.pipe(uglify())
+		.pipe(rename('app.min.js'))
+		.pipe(gulp.dest(dest.js));
+});
+
+/* Compress CSS */
+gulp.task('compress:css', function() {
+	return gulp.src(dest.css + 'style.css')
+		.pipe(minifyCss())
+        .pipe(rename('style.min.css'))
+		.pipe(gulp.dest(dest.css));
+});
+
+/* Server with auto reload and browersync */
+gulp.task('serve', ['build'], function () {
+      browserSync({
+        notify: false,
+        // https: true,
+        server: [outputDir],
+        tunnel: false
+      });
+
+      gulp.watch([src.html + '**/*.html'], ['html', reload]);
+      gulp.watch([src.css + '**/*.scss'], ['css', reload]);
+      gulp.watch([src.img + '*'], ['img', reload]);
+      gulp.watch([src.js + '**/*'], ['js', reload]);
+});
+
+/* Watch */
+gulp.task('watch', function () {
+      gulp.watch([src.html + '**/*.html'], ['html']);
+      gulp.watch([src.css + '**/*.scss'], ['css']);
+      gulp.watch([src.img + '**/*'], ['img']);
+      gulp.watch([src.js + '**/*'], ['js']);
+});
+
+/* Page speed insights */
+gulp.task('psi', function(cb) {
+  pagespeed.output(publicUrl, {
+    strategy: psiStrategy,
+  }, cb);
+});
+
+
+/************************
+ *  Task API
+ ************************/
+/* Start task */
+gulp.task('start', ['html', 'css', 'js', 'img', 'font', 'serve']);
+
+/* Final build task including compression */
+gulp.task('build', ['html', 'css', 'js', 'compress']);
+
+/* The compress task */
+gulp.task('compress', ['compress:css']);
+
+/* Default 'refresh' task */
+gulp.task('default', ['start']);
---
+++ app/templates/src/js/app.js
@@ -0,0 +1,65 @@
+//Closure to encapsulate all JS
+var UTILS = {
+		assign: require('object-assign'),
+		merge: require('merge'),
+		attributelist: require('storm-attributelist'),
+		classlist: require('dom-classlist'),
+		loadScript: require('load-script')
+	},
+    UI = (function(w, d) {
+            'use strict';
+
+            var ffo = require('FontFaceObserver'),
+                picturefill = require('picturefill'),
+                Toggler = require('storm-toggler'),
+                initFonts = function(){
+                    var ffo = new FontFaceObserver('Name of your font', {})
+                        .check()
+                        .then(function () {
+                            d.documentElement.className = document.documentElement.className.replace(/\bno-webfonts\b/,'');
+                        }, function () {
+                            console.log('Font is not available after waiting 5 seconds');
+                        });
+                },
+                initForrms = function(){
+                    //detect and return if not needed
+                    if(!(d.querySelector('.js-forrm'))) { return; } 
+                    //loaded async as required
+                    UTILS.loadScript('/content/js/async/forrm.min.js', function(err){
+                        if(err) {
+                            return console.log(err);
+                        }
+                        Forrm.init('.js-forrm');
+                    });
+                },
+				initTogglers = function() {
+                    if(!(d.querySelector('.js-toggle'))) { return; } 
+					Toggler.init('.js-toggle');
+                    Toggler.init('.js-toggle-local', {targetLocal: true});
+				},
+				load = function(){},
+                init = function() {
+                    //initialise everything
+                    initFonts();
+                    initForrms();
+                    initTogglers();
+                };
+
+            //Interface with/entry point to site JS
+            return {
+                init: init,
+				load: load
+            };
+
+        }(window, document, undefined));
+
+//expose globally
+global.STORM = {
+    UTILS: UTILS,
+    UI: UI
+};
+
+//Cut the mustard
+//Don't run any JS if the browser can't handle it
+if('addEventListener' in window) window.addEventListener('DOMContentLoaded', UI.init, false);
+if('addEventListener' in window) window.addEventListener('load', UI.load, false);
